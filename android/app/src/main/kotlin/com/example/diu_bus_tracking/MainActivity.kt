bd1ef98d8538e7aa4c4f878710035db35bb342f5
shukur-alom <shukuralom1234@gmail.com>
2024-11-06 10:23:12 +0600

    INIT

--- android/app/src/main/kotlin/com/example/diu_bus_tracking/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.diu_bus_tracking
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/model/mqtt_model.dart
+++
@@ -1,5 +0,0 @@
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
-
-class MqttModel {
-  RxString message = ''.obs;
-}
